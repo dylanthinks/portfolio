b47edcfb924df6a0640caa61e626101c7a51aaec
Dylan <dylanthinks@gmail.com>
2022-02-09 22:46:44 -0600

    linking together pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <Link to="/">Back to Home</Link>
+            <h1>About Me</h1>
+            <p></p>
+        </main>
+    )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import * as React from "react"
+import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <div>
       <title>Dylan Hinks</title>
+      <Link to="/about">About</Link>
       <h1>Welcome to my site</h1>
       <p>Getting back in the groove</p>
     </div>

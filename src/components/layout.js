203a079e87a8e153fa6ffcfa10792295ffca7806
Dylan <dylanthinks@gmail.com>
2022-02-10 23:13:38 -0600

    styled components global styles initialized, can go forward now

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,59 @@
 import * as React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import { Link } from 'gatsby'
+
+const GlobalStyle = createGlobalStyle`
+html {
+  line-height: 1.15;
+  -webkit-text-size-adjust: 100%; 
+}
+
+body, ul {
+    color: ${props => (props.theme === "purple" ? "purple" : "white")};
+    margin: 0;
+    width: 100%;
+    height: 100%;
+}
+
+main {
+  display: block;
+}
+
+h1 {
+  font-size: 2em;
+  margin: 0.67em 0;
+}
+
+b,
+strong {
+  font-weight: bolder;
+}
+
+code,
+kbd,
+samp {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+`
 
 const Layout = ({ pageTitle, children }) => {
     return (
-        <div>
-            <title>{pageTitle}</title>
+        <React.Fragment>
+        <main>
+        <GlobalStyle theme="purple" />
+            <title>{pageTitle}</title> 
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                 </ul>
             </nav>
-            <main>
-                <h1>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
+            <h1>{pageTitle}</h1> 
+        {children}
+        </main>
+         </React.Fragment>
     )
-}
+} 
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import Layout from '../components/layout'
+
+const Container = styled.div`
+background-color: grey;
+`
 
 const IndexPage = () => {
   return (
+    <Container>
    <Layout pageTitle="Home Page">
      <p>ball rolling again. Dusting off my fingers</p>
    </Layout>
+   </Container>
   )
 }
 

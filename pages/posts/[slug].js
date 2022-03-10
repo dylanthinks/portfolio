3e7edc3ab23af52aed3fcd2cbf21980b9f09caa5
Dylan <dylanthinks@gmail.com>
2022-03-10 00:27:03 -0600

    final barebones blog is fully up, now to deploy it live and then work on design system next

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,15 +1,27 @@
 import groq from 'groq'
 import imageUrlBuilder from '@sanity/image-url'
+import {PortableText} from '@portabletext/react'
 import client from '../../client'
-/* import Date from '../../components/date'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-*/
-import Layout from '../../components/layout'
-import Head from 'next/head'
-import utilStyles from '../../styles/utils.module.css'
 
 function urlFor (source) {
   return imageUrlBuilder(client).image(source)
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img 
+        alt={value.alt || ' '}
+        loading="lazy"
+        src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+        />
+      )
+    }
+  }
 }
 
 const Post = ({post}) => {
@@ -17,7 +29,8 @@ const Post = ({post}) => {
     title = 'Missing title', 
     name = 'Missing name', 
     categories,
-    authorImage 
+    authorImage,
+    body = []
   } = post
   return (
     <article>
@@ -35,9 +48,14 @@ const Post = ({post}) => {
           src={urlFor(authorImage)
           .width(250)
           .url()}
+          alt={`${name}'s picture`}
           />
         </div>
       )}
+      <PortableText
+      value={body}
+      components={ptComponents}
+      />
     </article>
   )
 }
@@ -46,26 +64,9 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
   "name": author->name,
   "categories": categories[]->title,
-  "authorImage": author->image
+  "authorImage": author->image,
+  body
 }`
-/*
-export default function Post({ postData }) {
-  return (
-  <Layout>
-  <Head>
-    <title>{postData.title}</title>
-  </Head>
-    <article>
-      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-      <div className={utilStyles.lightText}>
-      <Date dateString={postData.date} />
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
-  </Layout>
-  )
-}
-*/
 
 export async function getStaticPaths() {
   const paths = await client.fetch(
@@ -78,15 +79,6 @@ export async function getStaticPaths() {
   }
 }
 
-/*
-export async function getStaticPaths() {
-  const paths = getAllPostIds() 
-  return {
-    paths,
-    fallback: false
-  }
-}
-*/
 
 export async function getStaticProps(context) {
   const { slug = "" } = context.params
@@ -97,16 +89,5 @@ export async function getStaticProps(context) {
     }
   }
 }
-
-/*
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
-}
-*/
 
 export default Post

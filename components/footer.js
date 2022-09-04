2cc3da996fe27c808ffb48dc69f6fce5565e1e23
Dylan <dylanthinks@gmail.com>
2022-09-04 17:14:24 -0500

    footer fixed

--- components/footer.js
+++ components/footer.js
@@ -23,26 +23,6 @@ const Footer = () => {
               >
                 LinkedIn
               </a>
-              </li>
-          </div>
-          <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
-            <a
-              href="https://www.linkedin.com/in/dylanhinks/"
-              className={"transition-colors hover:text-yellow-500"}
-              target="_blank"
-              rel="noreferrer"
-            >
-              LinkedIn
-            </a>
-            <a
-              href="https://github.com/dylanthinks"
-              className="transition-colors hover:text-yellow-500"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </div>
             </li>
             <li>
               <a
@@ -53,7 +33,7 @@ const Footer = () => {
                 GitHub
               </a>
             </li>
-            </ul>
+          </ul>
         </div>
       </div>
     </footer>
